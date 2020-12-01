f817ad34d35e90d7b917851668ec335a3666ea87
maszrum <grzyb3k@hotmail.com>
2020-12-01 14:53:02 +0100

    added GetMyModel query

---
+++ MySample.Application/Queries/GetMyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+using MySample.Application.ViewModels;
+
+namespace MySample.Application.Queries
+{
+    public class GetMyModel : IRequest<MyModelViewModel>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ MySample.Application/Queries/GetMyModelHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MySample.Application.ViewModels;
+using MySample.Core.Models;
+
+namespace MySample.Application.Queries
+{
+    internal class GetMyModelHandler : IRequestHandler<GetMyModel, MyModelViewModel>
+    {
+        public Task<MyModelViewModel> Handle(GetMyModel request, CancellationToken cancellationToken)
+        {
+            // validate request
+            
+            // read model from repository
+            var model = MyModel.CreateNew();
+            
+            // map model to view model
+            return Task.FromResult(new MyModelViewModel());
+        }
+    }
+}
