b9ed5d49e830517cdb59261c32bd42a24390a341
maszrum <grzyb3k@hotmail.com>
2020-12-02 00:11:41 +0100

    renamed class

---
+++ MySample.Core/Shared/ModelPropertiesModifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MySample.Core.Events;
+
+namespace MySample.Core.Shared
+{
+    public class ModelPropertiesModifier<T> : IAsyncDisposable where T : AggregateRoot<T>
+    {
+        private readonly T _model;
+        private readonly List<string> _propertiesModified = new List<string>();
+
+        public ModelPropertiesModifier(T model)
+        {
+            _model = model;
+        }
+        
+        public bool IsDisposed { get; private set; }
+
+        public void AppendPropertyModified(string propertyName)
+        {
+            if (!_propertiesModified.Contains(propertyName))
+            {
+                _propertiesModified.Add(propertyName);
+            }
+        }
+        
+        public async ValueTask DisposeAsync()
+        {
+            IsDisposed = true;
+            
+            var @event = new ModelChanged<T>(_model, _propertiesModified);
+            await DomainEvents.Publish(@event);
+        }
+    }
+}
