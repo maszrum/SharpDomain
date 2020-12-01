aaa9c06d9a0eae04e3506222c13374e3285e7d59
maszrum <grzyb3k@hotmail.com>
2020-12-01 15:39:46 +0100

    get objects from repository

--- MySample.Application/Queries/GetMyModelHandler.cs
+++ MySample.Application/Queries/GetMyModelHandler.cs
@@ -1,22 +1,34 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using MySample.Application.Exceptions;
 using MySample.Application.ViewModels;
+using MySample.Core.InfrastructureInterfaces;
 using MySample.Core.Models;
 
 namespace MySample.Application.Queries
 {
     internal class GetMyModelHandler : IRequestHandler<GetMyModel, MyModelViewModel>
     {
-        public Task<MyModelViewModel> Handle(GetMyModel request, CancellationToken cancellationToken)
+        private readonly IMyModelReadRepository _repository;
+
+        public GetMyModelHandler(IMyModelReadRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<MyModelViewModel> Handle(GetMyModel request, CancellationToken cancellationToken)
         {
             // validate request
             
-            // read model from repository
-            var model = MyModel.CreateNew();
-            
+            var model = await _repository.Get(request.Id);
+            if (model is null)
+            {
+                throw new ObjectNotFoundException<MyModel>(request.Id);
+            }
+
             // map model to view model
-            return Task.FromResult(new MyModelViewModel());
+            return new MyModelViewModel();
         }
     }
 }
