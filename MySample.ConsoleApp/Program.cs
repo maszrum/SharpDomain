31173de9d7cd9c143bbbebab58fd699f57e6ad44
maszrum <grzyb3k@hotmail.com>
2020-12-01 12:12:16 +0100

    initial commit

---
+++ MySample.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MySample.ConsoleApp
+{
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
