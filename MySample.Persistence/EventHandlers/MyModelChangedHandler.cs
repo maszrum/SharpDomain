2d8300fbae4bf30b835dde843b94579d20659d93
maszrum <grzyb3k@hotmail.com>
2020-12-01 17:49:20 +0100

    added on change event handler

---
+++ MySample.Persistence/EventHandlers/MyModelChangedHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MySample.Core.Events;
+using MySample.Core.InfrastructureInterfaces;
+using MySample.Core.Models;
+
+namespace MySample.Persistence.EventHandlers
+{
+    // ReSharper disable once UnusedType.Global
+    internal class MyModelChangedHandler : INotificationHandler<ModelChanged<MyModel>>
+    {
+        private readonly IMyModelWriteRepository _repository;
+
+        public MyModelChangedHandler(IMyModelWriteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(ModelChanged<MyModel> notification, CancellationToken cancellationToken) => 
+            _repository.Update(notification.Model);
+    }
+}
