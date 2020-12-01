d21edc3805a338839aeee0675dd6c5cfd7553870
maszrum <grzyb3k@hotmail.com>
2020-12-01 17:36:02 +0100

    added autofac and event handlers

---
+++ MySample.Persistence/AutofacExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using MediatR;
+using MySample.Persistence.Entities;
+
+namespace MySample.Persistence
+{
+    public static class AutofacExtensions
+    {
+        public static ContainerBuilder RegisterPersistenceLayer(this ContainerBuilder containerBuilder)
+        {
+            return containerBuilder
+                .RegisterPersistenceHandlers();
+        }
+        
+        private static ContainerBuilder RegisterPersistenceHandlers(this ContainerBuilder containerBuilder)
+        {
+            static bool IsNotificationHandler(Type t) =>
+                t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>));
+            
+            var assembly = typeof(MyModelEntity).GetTypeInfo().Assembly;
+
+            var notificationHandlerTypes = assembly.DefinedTypes
+                .Where(IsNotificationHandler)
+                .Select(t => (Type)t)
+                .ToArray();
+            
+            containerBuilder
+                .RegisterTypes(notificationHandlerTypes)
+                .AsImplementedInterfaces();
+            
+            return containerBuilder;
+        }
+    }
+}
---
+++ MySample.Persistence/EventHandlers/MyModelCreatedHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MySample.Core.Events;
+using MySample.Core.InfrastructureInterfaces;
+
+namespace MySample.Persistence.EventHandlers
+{
+    internal class MyModelCreatedHandler : INotificationHandler<MyModelCreated>
+    {
+        private readonly IMyModelWriteRepository _repository;
+
+        public MyModelCreatedHandler(IMyModelWriteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(MyModelCreated notification, CancellationToken cancellationToken) => 
+            _repository.Create(notification.Model);
+    }
+}
