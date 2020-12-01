85f8f5cf1e5ccbf5ae760e282e36494d23b0c36e
maszrum <grzyb3k@hotmail.com>
2020-12-01 15:26:25 +0100

    added application exception

---
+++ MySample.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySample.Application.Exceptions
+{
+    internal abstract class ApplicationException : Exception
+    {
+        protected ApplicationException(string? message) 
+            : base(message)
+        {
+        }
+
+        protected ApplicationException(string? message, Exception? innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ MySample.Application/Exceptions/ObjectNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySample.Application.Exceptions
+{
+    internal class ObjectNotFoundException<T> : ApplicationException
+    {
+        public Type ObjectType { get; }
+        public Guid Id { get; }
+        
+        public ObjectNotFoundException(Guid id) 
+            : base($"object with id {id} of type {typeof(T).Name} was not found")
+        {
+            ObjectType = typeof(T);
+            Id = id;
+        }
+    }
+}
