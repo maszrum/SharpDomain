01070071e2ec0ba0345d087c1de0ecd26b99a76a
maszrum <grzyb3k@hotmail.com>
2020-12-01 17:38:10 +0100

    register mediatr handlers to container

--- MySample.Application/AutofacExtensions.cs
+++ MySample.Application/AutofacExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Autofac;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
 using MediatR;
 using MySample.Core;
 
@@ -9,7 +12,9 @@ namespace MySample.Application
         public static ContainerBuilder RegisterApplicationLayer(this ContainerBuilder containerBuilder)
         {
             return containerBuilder
-                .RegisterMediatR();
+                .RegisterMediatR()
+                .RegisterRequestHandlers()
+                .RegisterNotificationHandlers();
         }
         
         private static ContainerBuilder RegisterMediatR(this ContainerBuilder containerBuilder)
@@ -29,6 +34,44 @@ namespace MySample.Application
                 var mediator = context.Resolve<IMediator>();
                 DomainEvents.Init(mediator);
             });
+            
+            return containerBuilder;
+        }
+        
+        private static ContainerBuilder RegisterRequestHandlers(this ContainerBuilder containerBuilder)
+        {
+            static bool IsRequestHandler(Type t) =>
+                t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>));
+            
+            var assembly = typeof(AutofacExtensions).GetTypeInfo().Assembly;
+
+            var requestHandlerTypes = assembly.DefinedTypes
+                .Where(IsRequestHandler)
+                .Select(t => (Type)t)
+                .ToArray();
+            
+            containerBuilder
+                .RegisterTypes(requestHandlerTypes)
+                .AsImplementedInterfaces();
+            
+            return containerBuilder;
+        } 
+        
+        private static ContainerBuilder RegisterNotificationHandlers(this ContainerBuilder containerBuilder)
+        {
+            static bool IsNotificationHandler(Type t) =>
+                t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>));
+            
+            var assembly = typeof(AutofacExtensions).GetTypeInfo().Assembly;
+
+            var notificationHandlerTypes = assembly.DefinedTypes
+                .Where(IsNotificationHandler)
+                .Select(t => (Type)t)
+                .ToArray();
+            
+            containerBuilder
+                .RegisterTypes(notificationHandlerTypes)
+                .AsImplementedInterfaces();
             
             return containerBuilder;
         }
