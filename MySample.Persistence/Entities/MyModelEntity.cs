f5ac68e0599a115cfae2bd5127d3363780a5cd3e
maszrum <grzyb3k@hotmail.com>
2020-12-01 15:14:56 +0100

    added persistence entity for MyModel

---
+++ MySample.Persistence/Entities/MyModelEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MySample.Persistence.Entities
+{
+    public class MyModelEntity
+    {
+        public Guid Id { get; set; }
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+    }
+}
