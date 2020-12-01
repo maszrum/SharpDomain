8b2882293b534c3f1ac00a6032e740220a410502
maszrum <grzyb3k@hotmail.com>
2020-12-01 14:52:25 +0100

    added repositories interfaces

---
+++ MySample.Application/InfrastructureInterfaces/IMyModelReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySample.Core.Models;
+
+namespace MySample.Application.InfrastructureInterfaces
+{
+    internal interface IMyModelReadRepository
+    {
+        Task<MyModel> Get(Guid id);
+    }
+}
---
+++ MySample.Application/InfrastructureInterfaces/IMyModelWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySample.Core.Models;
+
+namespace MySample.Application.InfrastructureInterfaces
+{
+    internal interface IMyModelWriteRepository
+    {
+        Task<MyModel> Create(MyModel model);
+        Task Update(MyModel model);
+        Task Delete(Guid id);
+    }
+}
