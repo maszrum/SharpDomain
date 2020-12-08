4ab0fe7f48fe25c9ffa238bf45715c3c4f7af262
maszrum <grzyb3k@hotmail.com>
2020-12-08 16:33:20 +0100

    added pesel value object

---
+++ Sample/VotingSystem.Core/ValueObjects/Pesel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VotingSystem.Core.ValueObjects
+{
+    public class Pesel : IEquatable<Pesel>
+    {
+        public Pesel(string pesel)
+        {
+            Code = pesel;
+        }
+
+        public string Code { get; }
+
+        public bool Equals(Pesel? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Code, other.Code, StringComparison.InvariantCulture);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Pesel) obj);
+        }
+
+        public override int GetHashCode() => StringComparer.InvariantCulture.GetHashCode(Code);
+        
+        public static Pesel Create(string pesel)
+        {
+            // TODO: validate pesel
+
+            return new(pesel);
+        }
+    }
+}
