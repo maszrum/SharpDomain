7e0a5b21d56591fb98f7a2726faf54ffbe0eb795
maszrum <grzyb3k@hotmail.com>
2020-12-08 16:34:10 +0100

    added properties to models

--- Sample/VotingSystem.Core/Models/Voter.cs
+++ Sample/VotingSystem.Core/Models/Voter.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using SharpDomain.Core;
 using VotingSystem.Core.Events;
+using VotingSystem.Core.ValueObjects;
 
 namespace VotingSystem.Core.Models
 {
     public class Voter : AggregateRoot<Voter>
     {
-        public Voter(Guid id)
+        public Voter(Guid id, Pesel pesel)
         {
             Id = id;
+            Pesel = pesel;
         }
 
         public override Guid Id { get; }
+        
+        public Pesel Pesel { get; }
 
-        public static IDomainResult<Voter> Create()
+        public static IDomainResult<Voter> Create(string pesel)
         {
             var id = Guid.NewGuid();
-            var model = new Voter(id);
+            var peselValue = new Pesel(pesel);
+            
+            var model = new Voter(id, peselValue);
             
             var createdEvent = new VoterCreated(model);
             
