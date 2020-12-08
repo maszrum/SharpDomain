4cca561571c0160a5be45bd02bd2c498cd16d1dd
maszrum <grzyb3k@hotmail.com>
2020-12-08 16:34:10 +0100

    added properties to models

--- Sample/VotingSystem.Core/Models/Vote.cs
+++ Sample/VotingSystem.Core/Models/Vote.cs
@@ -6,17 +6,23 @@ namespace VotingSystem.Core.Models
 {
     public class Vote : AggregateRoot<Vote>
     {
-        public Vote(Guid id, Guid voterId)
+        public Vote(Guid id, Guid voterId, Guid questionAnswerId)
         {
             Id = id;
+            VoterId = voterId;
+            QuestionAnswerId = questionAnswerId;
         }
 
         public override Guid Id { get; }
+        
+        public Guid VoterId { get; }
 
-        public static IDomainResult<Vote> Create(Guid voterId)
+        public Guid QuestionAnswerId { get; }
+        
+        public static IDomainResult<Vote> Create(Guid voterId, Guid questionAnswerId)
         {
             var id = Guid.NewGuid();
-            var model = new Vote(id, voterId);
+            var model = new Vote(id, voterId, questionAnswerId);
             
             var createdEvent = new VotePosted(model);
             
