ceacb1154af4699728a6617468556e3042abfd25
maszrum <grzyb3k@hotmail.com>
2020-12-09 13:07:48 +0100

    created abstractions for write repositories

---
+++ Sample/VotingSystem.Persistence/RepositoryInterfaces/IAnswersWriteRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using VotingSystem.Persistence.Entities;
+
+namespace VotingSystem.Persistence.RepositoryInterfaces
+{
+    public interface IAnswersWriteRepository
+    {
+        Task Update(AnswerResultEntity entity);
+    }
+}
