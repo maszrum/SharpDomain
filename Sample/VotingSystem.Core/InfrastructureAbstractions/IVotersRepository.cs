18daa8523e8170c2813d4e830fc04a7885447395
maszrum <grzyb3k@hotmail.com>
2020-12-09 11:21:08 +0100

    added methods to voters repository

--- Sample/VotingSystem.Core/InfrastructureAbstractions/IVotersRepository.cs
+++ Sample/VotingSystem.Core/InfrastructureAbstractions/IVotersRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace VotingSystem.Core.InfrastructureAbstractions
+﻿using System;
+using System.Threading.Tasks;
+using VotingSystem.Core.Models;
+using VotingSystem.Core.ValueObjects;
+
+namespace VotingSystem.Core.InfrastructureAbstractions
 {
     public interface IVotersRepository
     {
+        Task<Voter?> GetVoterByPesel(Pesel pesel);
+        Task<bool> VoterExists(Guid voterId);
     }
 }
