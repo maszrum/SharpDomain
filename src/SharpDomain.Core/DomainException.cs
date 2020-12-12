e3242281bcbaa564cebb5e313f3bc1c7e8ce618e
maszrum <grzyb3k@hotmail.com>
2020-12-12 19:49:07 +0100

    added protected constructors

--- src/SharpDomain.Core/DomainException.cs
+++ src/SharpDomain.Core/DomainException.cs
@@ -4,5 +4,14 @@ namespace SharpDomain.Core
 {
     public abstract class DomainException : Exception
     {
+        protected DomainException(string? message)
+            : base(message)
+        {
+        }
+
+        protected DomainException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
     }
 }
