4ed73d369b0ab719752e59f48fa10eeedd26dbda
maszrum <grzyb3k@hotmail.com>
2021-02-10 10:21:52 +0100

    added system initialization

---
+++ src/SharpDomain.IoC/ISystemInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SharpDomain.IoC
+{
+    public interface ISystemInitializer
+    {
+        Task InitializeIfNeed();
+        Task InitializeForcefully();
+    }
+}
---
+++ src/SharpDomain.IoC/InitializeExtension.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace SharpDomain.IoC
+{
+    public static class InitializeExtension
+    {
+        public static TBuilder InitializeIfNeed<TBuilder>(this TBuilder systemBuilder) 
+            where TBuilder : SystemBuilder
+        {
+            systemBuilder.OnBuild(context =>
+            {
+                var initializers = context.Resolve<IEnumerable<ISystemInitializer>>();
+                return InitializeIfNeed(initializers);
+            });
+            
+            return systemBuilder;
+        }
+        
+        public static TBuilder InitializeForcefully<TBuilder>(this TBuilder systemBuilder) 
+            where TBuilder : SystemBuilder
+        {
+            systemBuilder.OnBuild(context =>
+            {
+                var initializers = context.Resolve<IEnumerable<ISystemInitializer>>();
+                return InitializeForcefully(initializers);
+            });
+            
+            return systemBuilder;
+        }
+        
+        private static async Task InitializeIfNeed(IEnumerable<ISystemInitializer> initializers)
+        {
+            foreach (var initializer in initializers)
+            {
+                await initializer.InitializeIfNeed();
+            }
+        }
+        
+        private static async Task InitializeForcefully(IEnumerable<ISystemInitializer> initializers)
+        {
+            foreach (var initializer in initializers)
+            {
+                await initializer.InitializeForcefully();
+            }
+        }
+    }
+}
